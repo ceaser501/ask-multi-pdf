4212a9f90db8bbceb6352623995a4dec06266fcf
Taesu,Kim <ceaser501@naver.com>
2023-06-22 17:27:35 +0900

    test2

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def handle_userinput(user_question):
 
 def main():
     # load_dotenv()
-    st.set_page_config(page_title="Chat with multiple PDFs!!!",
+    st.set_page_config(page_title="Chat with multiple PDFs",
                        page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
 
@@ -74,7 +74,7 @@ def main():
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
 
-    st.header("Chat with multiple PDFs :books:")
+    st.header("Chat with multiple PDFs!!! :books:")
     user_question = st.text_input("Ask a question about your documents:")
     if user_question:
         handle_userinput(user_question)
